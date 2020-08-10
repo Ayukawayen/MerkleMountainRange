099d8f337421a6afafec78845871d127da6c0e01
Ayukawayen <Ayukawayen@Gmail.com>
2020-08-10 13:28:57 +0800

    feat(Contract): update bounty

--- Contract/bounty.js
+++ Contract/bounty.js
@@ -1,11 +1,10 @@
 pragma solidity >=0.6.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.1.0/contracts/token/ERC20/IERC20.sol";
-
-interface MMR is IERC20 {
+interface MMR {
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 	function mint() external payable;
 	function tokenPrice() external view returns (uint);
-	function verifyOnChain(uint blockNumber, bytes32 blockHash, bytes32[] calldata proof, uint localBlockNumber, bytes32[8] calldata localRoots) external returns (bool);
+	function verifyOnChain(uint blockNumber, bytes32 blockHash, bytes32[] calldata proof, uint localBlockNumber, bytes32[8] calldata localRoots) external returns (bool, string memory);
 }
 
 contract Bounty {
@@ -14,6 +13,9 @@ contract Bounty {
 	MMR _mmr;
     constructor (address addr) public payable {
         _mmr = MMR(addr);
+    }
+    receive() external payable {
+        
     }
 	
 	function challengeWithToken(uint blockNumber, bytes32 blockHash1, bytes32[] memory proof1, uint localBlockNumber1, bytes32[8] memory localRoots1, bytes32 blockHash2, bytes32[] memory proof2, uint localBlockNumber2, bytes32[8] memory localRoots2) public {
@@ -35,13 +37,15 @@ contract Bounty {
 	}
 	
 	function challenge(uint blockNumber, bytes32 blockHash1, bytes32[] memory proof1, uint localBlockNumber1, bytes32[8] memory localRoots1, bytes32 blockHash2, bytes32[] memory proof2, uint localBlockNumber2, bytes32[8] memory localRoots2) internal {
-		require(blockHash1 != bytes32(0));
-		require(blockHash2 != bytes32(0));
+		require(blockHash1 != bytes32(0), "blockHash1 is ZERO");
+		require(blockHash2 != bytes32(0), "blockHash2 is ZERO");
 		
-		//require(blockHash1 != blockHash2);
+		require(blockHash1 != blockHash2, "blockHash1 and blockHash2 are equal");
 		
-		require(_mmr.verifyOnChain(blockNumber, blockHash1, proof1, localBlockNumber1, localRoots1));
-		require(_mmr.verifyOnChain(blockNumber, blockHash2, proof2, localBlockNumber2, localRoots2));
+		(bool isVerifed1,) = _mmr.verifyOnChain(blockNumber, blockHash1, proof1, localBlockNumber1, localRoots1);
+		require(isVerifed1, "blockHash1 not verified");
+		(bool isVerifed2,) = _mmr.verifyOnChain(blockNumber, blockHash2, proof2, localBlockNumber2, localRoots2);
+		require(isVerifed2, "blockHash2 not verified");
 		
 		msg.sender.transfer(address(this).balance);
 	}
