ef19456e948227dc44c221c7f20b149cc84727da
Ayukawayen <Ayukawayen@Gmail.com>
2020-08-13 14:44:26 +0800

    feat(Contract): update bounty

--- Contract/bounty.js
+++ Contract/bounty.js
@@ -4,19 +4,28 @@ interface MMR {
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 	function mint() external payable;
 	function tokenPrice() external view returns (uint);
-	function verifyOnChain(uint blockNumber, bytes32 blockHash, bytes32[] calldata proof, uint localBlockNumber, bytes32[8] calldata localPeaks) external returns (bool, string memory);
+	function verifyOnChain(uint blockNumber, bytes32 blockHash, bytes32[] calldata proof, uint localBlockNumber, bytes32[8] calldata localPeaks) external returns (bool);
 }
 
 contract Bounty {
 	uint internal constant VERIFY_COST = 6000;
 	
 	MMR _mmr;
+	address payable _owner;
+	uint _createTime;
+	
     constructor (address addr) public payable {
         _mmr = MMR(addr);
+		_owner = msg.sender;
+		_createTime = block.timestamp;
     }
     receive() external payable {
-        
     }
+	function destruct() public {
+		require(block.timestamp > _createTime + 60 days);
+		
+		selfdestruct(_owner);
+	}
 	
 	function challengeWithToken(uint blockNumber, bytes32 blockHash1, bytes32[] memory proof1, uint localBlockNumber1, bytes32[8] memory localPeaks1, bytes32 blockHash2, bytes32[] memory proof2, uint localBlockNumber2, bytes32[8] memory localPeaks2) public {
 		_mmr.transferFrom(msg.sender, address(this), VERIFY_COST*2);
@@ -42,10 +51,8 @@ contract Bounty {
 		
 		require(blockHash1 != blockHash2, "blockHash1 and blockHash2 are equal");
 		
-		(bool isVerifed1,) = _mmr.verifyOnChain(blockNumber, blockHash1, proof1, localBlockNumber1, localPeaks1);
-		require(isVerifed1, "blockHash1 not verified");
-		(bool isVerifed2,) = _mmr.verifyOnChain(blockNumber, blockHash2, proof2, localBlockNumber2, localPeaks2);
-		require(isVerifed2, "blockHash2 not verified");
+		require(_mmr.verifyOnChain(blockNumber, blockHash1, proof1, localBlockNumber1, localPeaks1), "blockHash1 not verified");
+		require(_mmr.verifyOnChain(blockNumber, blockHash2, proof2, localBlockNumber2, localPeaks2), "blockHash2 not verified");
 		
 		msg.sender.transfer(address(this).balance);
 	}
